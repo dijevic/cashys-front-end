94d547a0658f807f879bc168204300ef10d10af0
[diego] <[dielmac7@gmail.com]>
2022-03-16 21:42:10 -0300

    logout button added

--- src/components/ui/navbar/NavBar.js
+++ src/components/ui/navbar/NavBar.js
@@ -4,10 +4,11 @@ import React from 'react'
 // styled components imp
 import { theme } from '../../../styles/theme'
 import { Link } from '../../coomon/buttom/Link'
-import { StyledNav, Div, Span } from './styles'
+import { StyledNav, Div, Span, DivLogout } from './styles'
 import { UserIcon } from '../../coomon/icons/User'
 import { BrandLogo } from '../../coomon/logo'
 import { useUserStore } from '../../../store/store'
+import { LogoutIcon } from '../../coomon/icons/LogoutIcon'
 
 
 const { buttonTypes } = theme
@@ -25,16 +26,25 @@ export const NavBar = () => {
             </Link>
             {
                 (user) &&
-                <Div>
-                    <Link
-                        type={buttonTypes.link}
-                        direction="column"
-                        centered="true"
-                        to="/app/profile">
-                        <UserIcon />
-                        <Span>Profile</Span>
-                    </Link>
-                </Div>
+                <>
+                    <Div>
+                        <Link
+                            type={buttonTypes.link}
+                            direction="column"
+                            centered="true"
+                            to="/app/profile">
+                            <UserIcon />
+                            <Span>Profile</Span>
+                        </Link>
+                        <DivLogout logout="true">
+                            <LogoutIcon />
+                            <p>log out</p>
+                        </DivLogout>
+                    </Div>
+
+
+
+                </>
 
             }
 
--- src/components/ui/navbar/styles.js
+++ src/components/ui/navbar/styles.js
@@ -19,14 +19,13 @@ padding:10px 100px;
 `
 
 
-
-
-
 export const Span = styled.span`
 border-bottom: 2px solid transparent;
 font-size: 1.3rem;
 line-height: 1.3rem;
 transition: .3s ease all;
+
+
 
 
 `
@@ -35,18 +34,56 @@ export const Div = styled.div`
 padding:2px 5px;
 width:fit-content;
 transition:.3s ease-in-out all ;
+display: flex;
+align-items: center;
+justify-content: center;
 @media(min-width:768px){
     svg{
     height:28px;
     width:28px;
 }
-&:hover{
-    transform: translateY(-4px);
+.icon-tabler-user{
+    transition: .3s ease all;
+}
+a{
+    &:hover{
     ${Span}{
+        transform: translateY(-4px);
         border-bottom: 2px solid ${colors.blue};
+    }
+    .icon-tabler-user{
+        transition: .3s ease all;
+        transform: translateY(-4px);
+
     }
 }
 }
+}
 
+`
+
+export const DivLogout = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-left: 20px;
+cursor: pointer;
+background: ${colors.white};
+padding: 5px 10px;
+border-radius: 10px;
+box-shadow: 5px 5px 7px rgba(0, 0, 0, 0.3);
+p{
+    font-size: 1.3rem;
+    line-height: 1.3rem;
+    border-bottom:2px solid transparent ;
+
+}
+
+&:hover{
+
+    p{
+        border-bottom:2px solid ${colors.purple} ;
+    }
+}
 `
 
