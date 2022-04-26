4f6c3bb6141ffcc60c911fb3468099a8644716b8
[diego] <[dielmac7@gmail.com]>
2022-04-25 22:44:45 -0300

    adding styles to filters

--- src/components/dashboard/styles.js
+++ src/components/dashboard/styles.js
@@ -175,7 +175,7 @@ export const DivContainer = styled.div`
 display: flex;
 align-items: center;
 justify-content: ${({ justify }) => (justify) && justify};
-
+max-width:80%;
 flex-wrap:${({ wrap }) => (wrap) ? wrap : 'wrap'};
 
 
